2d35d09c0a71285ec5161b5053d6357e5ee66fde
MrAnderson1971 <67478505+MrAnderson1971@users.noreply.github.com>
2020-12-28 18:05:29 -0800

    Update RightColumn.js

--- Desktop/multipageapp/src/RightColumn.js
+++ Desktop/multipageapp/src/RightColumn.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 class RightColumn extends Component {
     render() {
+
+        const questions = this.props.hw;
+
         return (
             <React.Fragment>
                 <p>Homework</p>
                 <div>
-                    {this.props.hw.map((name, index) => (<div>
-                        <h5>{index + 1}. {name.text}</h5>
-                        <textarea></textarea>
-                    </div>))}
+                    {
+                        Object.keys(questions).map((question, index) => (<div>
+                            <h5>{questions[question]}</h5>
+                            <textarea></textarea>
+                        </div>))
+                    }
 
                 </div>
             </React.Fragment>
