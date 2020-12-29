1b2ced8ebe40ff5b6e4783e28c5dc0a907cbab40
ricky <pneumoniccoma@gmail.com>
2020-12-29 15:48:43 -0800

    finished all api calls. We can get to designing and everything

--- Desktop/multipageapp/src/GetContent.js
+++ Desktop/multipageapp/src/GetContent.js
@@ -5,7 +5,7 @@ import './HomeScreen.css';
 import axios from 'axios';
 
 class GetContent extends Component {
-  constructor(props) {
+  constructor(props, pic) {
     super(props);
     
     
@@ -17,13 +17,90 @@ class GetContent extends Component {
     persons: [],
     titles: [],
     lesson: [],
-    
+    GodsLove: [],
+    books1: [],
+    FollowMe: [],
+    pictures: [],
+    name: [],
+    bookname: [],
+
+
+    parenting: [],
+    Chen: [],
+
   }
-  
-  submitHandler = (e) => {
-    e.preventDefault();
+  componentDidMount(){
+
     
     axios
+    .get("https://idigest.gtinternational.org/api/home", {
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY1NywibG9naW5JZCI6InBuZXVtb25pY2NvbWFAZ21haWwuY29tIiwiaWF0IjoxNjAzOTk4NjQwLCJleHAiOjE2MzU1MzQ2NDB9.rRXy4J5D5Rw9K2lTDTjd4VZ3hLEaBEU1BPNNND1HOys",
+        
+      },
+    })
+      .then((response) => {
+        
+        console.log(response.data.books);
+        
+        for (let i = 0; i < response.data.books.length; i++) {
+          
+          this.setState(prevState=> ({
+            pictures: [...prevState.pictures, response.data.books[i].image],
+            name: [...prevState.name, response.data.books[i].classNameChs],
+          }))
+
+
+          console.log("https://idigest.gtinternational.org/api/lesson/" + response.data.books[i].name);
+
+          axios
+            .get("https://idigest.gtinternational.org/api/lesson/" + response.data.books[i].name, {
+              headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY1NywibG9naW5JZCI6InBuZXVtb25pY2NvbWFAZ21haWwuY29tIiwiaWF0IjoxNjAzOTk4NjQwLCJleHAiOjE2MzU1MzQ2NDB9.rRXy4J5D5Rw9K2lTDTjd4VZ3hLEaBEU1BPNNND1HOys",
+                "Content-Type": "application/json",
+              },
+            })
+
+            .then((response) => {
+        
+              const GodsLove = response.data;
+              
+              
+              console.log(response.data.sessions);
+              
+              for (let i = 0; i < response.data.sessions.length; i++) {
+                
+                  this.setState(prevState=> ({
+                    titles: [...prevState.titles, response.data.sessions[i].title],
+                    lesson: [...prevState.lesson, response.data.sessions[i].lesson],
+                  }))
+                 
+                  
+                
+                
+                
+                
+              }
+              
+              
+            })
+            
+            .catch((error) => {
+              console.log(error);
+            });
+      
+        // console.log(response.data.sessions[i])
+        
+        
+      }
+      
+      })
+      
+      .catch((error) => {
+        console.log(error);
+      });
+
+      axios
       .get("https://idigest.gtinternational.org/api/lesson/GodsLove", {
         headers: {
           "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY1NywibG9naW5JZCI6InBuZXVtb25pY2NvbWFAZ21haWwuY29tIiwiaWF0IjoxNjAzOTk4NjQwLCJleHAiOjE2MzU1MzQ2NDB9.rRXy4J5D5Rw9K2lTDTjd4VZ3hLEaBEU1BPNNND1HOys",
@@ -31,13 +108,11 @@ class GetContent extends Component {
         },
       })
       .then((response) => {
-        console.log(response);
-        console.log(response.data);
-        console.log(response.data.title);
-        const persons = response.data;
+        
+        const GodsLove = response.data;
         
         
-        
+        console.log(response.data.sessions);
         
         for (let i = 0; i < response.data.sessions.length; i++) {
           
@@ -48,37 +123,74 @@ class GetContent extends Component {
            
             
           
-          console.log(response.data.sessions[i])
+          // console.log(response.data.sessions[i])
           
           
         }
-        this.setState({persons});
+        this.setState({GodsLove});
         //console.log(response.data.sessions[0]);
-        console.log(response.data.sessions[0]);
-        console.log(this.state.titles);
-        console.log(response.data.sessions[0].lesson);
+        // console.log(response.data.sessions[0]);
+        // console.log(this.state.titles);
+        // console.log(response.data.sessions[0].lesson);
         
       })
       
       .catch((error) => {
         console.log(error);
       });
+
+
+      axios
+      .get("https://idigest.gtinternational.org/api/lesson/FollowMe", {
+        headers: {
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY1NywibG9naW5JZCI6InBuZXVtb25pY2NvbWFAZ21haWwuY29tIiwiaWF0IjoxNjAzOTk4NjQwLCJleHAiOjE2MzU1MzQ2NDB9.rRXy4J5D5Rw9K2lTDTjd4VZ3hLEaBEU1BPNNND1HOys",
+          "Content-type": "application/json",
+        },
+      })
+      .then((response) => {
+        // console.log(response);
+        // console.log(response.data);
+        // console.log(response.data.title);
+        const FollowMe = response.data;
+        
+        
+        
+        
+        for (let i = 0; i < response.data.sessions.length; i++) {
+          
+            this.setState(prevState=> ({
+              books1: [...prevState.books1, response.data.sessions[i].title],
+              lesson: [...prevState.lesson, response.data.sessions[i].lesson],
+            }))
+           
+            
+          
+          // console.log(response.data.sessions[i])
+          
+          
+        }
+        this.setState({FollowMe});
+        //console.log(response.data.sessions[0]);
+        // console.log(response.data.sessions[0]);
+        // console.log(this.state.titles);
+        // console.log(response.data.sessions[0].lesson);
+        
+      })
+      
+      .catch((error) => {
+        console.log(error);
+      });
+   
   };
+    
+  
   
   render() {
     
     return (
       <div>
         
-        {/* <form onSubmit={this.submitHandler}>
-          
-          <h1>
-            {this.state.persons.title}
-          </h1>
-          <h3>{this.state.persons.description}</h3>
-          <button className="buttonsw">Submit</button>
-          
-        </form> */}
+        
         <h1>IDigest</h1>
         <img src="https://play-lh.googleusercontent.com/-DscnIMy5CY4pda2kulLTxfdPTRBRpYWK0OznZtEU6sYwaFkMWnco3udlX2JmrXidA0=s180" width="80px" height= "80px"></img>
         <h3>Welcome! Rickyzhao84</h3>
@@ -90,13 +202,13 @@ class GetContent extends Component {
     <p>Books</p>
     <section>
             
-            <a href = "/GodsLove">
-            <img className="photoss" src={"https://idigest.gtinternational.org/api/lesson/GodsLove/cover"} alt="God's Love"></img>
-            </a>
-            <br/>
-            <img className="photoss"src={"http://13.91.97.63/idigest/lesson/parenting/cover"} alt="God's Love"></img>
-            <br/>
-            <img className="photoss"src={"http://13.91.97.63/idigest/lesson/FollowMe/cover"} alt="God's Love"></img>
+            
+            <h5 class="left">{this.state.pictures.map(item => (
+            <a href="google.com"><img className="photoss"src={item} alt="God's Love"></img><ul key = {item}></ul></a>
+            
+            )
+          )}</h5>
+            
         </section>
         
   </div>
@@ -104,19 +216,25 @@ class GetContent extends Component {
     <p>Chapters</p>
     <form onSubmit={this.submitHandler}>
           
-          <h4>
-            {this.state.persons.title}
+          {/* <h4>
+            {this.state.GodsLove.title}
           </h4>
           
           <h4>
-            {this.state.persons.description}
+            {this.state.GodsLove.description}
             
-          </h4>
+          </h4> */}
+          <h5>{this.state.name.map(
+              item => (
+                <li key={item}>{item}</li>
+              )
+            )}</h5>
           {/* <h5>{this.state.persons.description}</h5> */}
         
           <h5 class="left">{this.state.titles.map(item => (
-            <a href="google.com"><li key = {item}>{item}</li></a>
             
+            <a href="google.com"><li key = {item}>{item}</li></a>
+            //CURRENTLYYYY WORKINGGG ON THIS
             )
           )}</h5>
           {/* <h5>{this.state.lesson.map(item => (
@@ -124,12 +242,27 @@ class GetContent extends Component {
             
             )
           )}</h5> */}
-          <div id="disappears">
-            
-          <button className="buttonsw">Read</button>
 
-          </div>
-          <p id="answers1"></p>
+         {/* <h2>-------------------------------------</h2>
+
+
+          <h5>
+            {this.state.FollowMe.title}
+          </h5>
+          
+          <h5>
+            {this.state.FollowMe.description}
+            
+          </h5>
+          <h5>{this.state.persons.description}</h5> 
+        
+          <h5 class="left">{this.state.books1.map(item => (
+            
+            <a href="google.com"><li key = {item}>{item}</li></a>
+            
+            )
+          )}</h5>
+           */}
         </form>
   </div>
   <div class="class3">
@@ -151,63 +284,3 @@ class GetContent extends Component {
 
   export default GetContent;
 
-
-/*
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          item: [],
-          isLoaded: false
-        };
-      }
-    
-      componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-          .then(res => res.json())
-          .then(json => {
-            this.setState({
-              isLoaded: true,
-              items: json,
-            })
-          });
-      }
-    /*
-      submitHandler = (e) => {
-        e.preventDefault();
-        console.log(this.state);
-        axios
-          .get("https://idigest.gtinternational.org/api/lesson/parenting?/content/*")
-          .then((response) => {
-            console.log(response);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
-    
-    *//*
-      render() {
-        var { isLoaded, items } = this.state;
-
-        if(!isLoaded) {
-          return <div>Loading!!</div>;
-        } else {
-        return (
-          <div>
-            <ul>
-              {items.map(item => (
-                <li key={item.id}>
-                  Name: {item.name} | Email: {item.email}
-                </li>
-              ))};
-            </ul>
-          </div>
-        );
-      }
-    }
-    }
-    
-export default GetContent;
-
-*/
